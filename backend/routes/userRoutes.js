588c76ba9eeb58ab96460899c83dec9277a29d84
Vivek Pramanik <39510579+icodervivek@users.noreply.github.com>
2025-08-02 01:59:10 +0530

    fix: updated the cookie based issue in the routing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -114,8 +114,8 @@ router.post("/signin", async (req, res) => {
     // Set token in HTTP-only cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "None",
     });
 
     // Send success response
