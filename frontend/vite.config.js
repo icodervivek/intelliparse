330fad0152b66744a30132f6b118d1b5562c7bd0
Vivek Pramanik <39510579+icodervivek@users.noreply.github.com>
2025-08-02 12:31:59 +0530

    fix: updated the vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
     base: '/',
   },
 });
